a9062178727252298979b216bc1ae863502b7bf6
fvonhoven <frank@infinite.red>
2019-07-24 17:04:49 -0500

    Change for heroku

--- resources/purchases/beforerequest.js
+++ resources/purchases/beforerequest.js
@@ -5,12 +5,10 @@ console.log(ctx.req.headers)
 
 if (event === "GET") {
   if (!userId) cancel("You must be authenticated view purchases", 401)
-  
   query.userId = userId
 }
 
 if (event === "POST") {
   if (!userId) cancel("You must be authenticated to purchase", 401)
-  
   this.userId = userId
 }
